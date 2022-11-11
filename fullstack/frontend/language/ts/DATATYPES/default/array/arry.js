371192b6ff88cc47a4db7d620d4c2b9050a8480b
wa5 <47422872+wa5@users.noreply.github.com>
2022-11-11 11:05:28 +0530

    jjjk

---
+++ fullstack/frontend/language/ts/DATATYPES/default/array/arry.js
@@ -0,0 +1,9 @@
+var arr1 = 'apple';
+var arr2 = 'boll';
+var arr3 = 'cat';
+var myarry = ['apple', 'boll', 'cat'];
+console.log(arr1);
+console.log(myarry[1]);
+//destructring 
+var apple = myarry[0], boll = myarry[1], cat = myarry[2];
+console.log(apple);
---
+++ fullstack/frontend/language/ts/functions/function.ts
@@ -0,0 +1,16 @@
+function dispaly(gift){
+    console.log(123,gift)
+
+}
+
+
+function marrage(gifts){
+    console.log(gifts)
+    return 'food'
+}
+
+alert(marrage(''))
+console.log(marrage('car'))
+console.log(marrage('watch'))
+console.log(marrage('helicopter'))
+
