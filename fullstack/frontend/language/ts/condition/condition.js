65cf2cbf9bcd23d336926b2b66dccc96e5920521
wa5 <47422872+wa5@users.noreply.github.com>
2022-11-14 09:33:21 +0530

    hhh

---
+++ fullstack/frontend/language/ts/condition/condition.js
@@ -0,0 +1,12 @@
+var a = 10;
+if (a == 10) {
+    console.log(a);
+}
+if (a < 10) {
+    console.log('a is less then 10');
+}
+else {
+    console.log('a is not less then 10');
+}
+;
+a <= 10 ? console.log('yes  a is equal rto 10') : '';
---
+++ fullstack/frontend/language/ts/loop/loop.js
@@ -0,0 +1,12 @@
+// var i
+// for(i=0;i<1000;i++){
+//     console.log(i)
+// }
+var j = 10;
+// while(j<20){
+//     console.log(j)
+//     j++
+// }
+do {
+    console.log(j);
+} while (j < 20);
