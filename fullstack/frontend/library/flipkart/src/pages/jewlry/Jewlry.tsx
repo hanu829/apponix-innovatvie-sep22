a0e6a0e5f783d87b6a36fbf4e485aaf62b1352b3
wa5 <47422872+wa5@users.noreply.github.com>
2022-11-24 09:22:53 +0530

    ll

--- fullstack/frontend/library/flipkart/src/pages/jewlry/Jewlry.tsx
+++ fullstack/frontend/library/flipkart/src/pages/jewlry/Jewlry.tsx
@@ -3,19 +3,44 @@ import { jewlrydata } from "../../api/jewlry.api"
 import { Card2 } from "../../components/mycards/Card2"
 
 
-interface Ijewlry{}
-export const Jewlry:React.FC<Ijewlry>=(props)=>{
-    var [localstate,setLocalstate]=useState([])
-    useEffect(()=>{
-        jewlrydata().then((a)=>{
+interface Ijewlry { }
+interface Irating {
+    count: number, rate: number
+}
+interface Ijuewlrypro {
+    category: string,
+    description: string,
+    id: number,
+    image: string
+    price: number
+    rating: Irating
+    title: string
+
+}
+export const Jewlry: React.FC<Ijewlry> = (props) => {
+    var [localstate, setLocalstate] = useState<Ijuewlrypro[]>([])
+    useEffect(() => {
+        jewlrydata().then((a) => {
+          console.log(a)
             setLocalstate(a)
-           
+
         })
-    },[])
-    
-   console.log(localstate)
+    }, [])
+
+
+  
+
+
+  
     return <>
-    <h1>category:</h1>
-    <Card2 description={"kkk"} image={"ll"} price={0} rating={{count:7,rate:6}} title={"mm"}/>
+        <h1>category:{localstate[0]?.category}</h1>
+       {localstate.map((p:any) => {
+        return <>
+            <Card2 description={p.description} image={p.image} price={p.price} rating={p.rating} title={p.title} />
+        </>
+        
+
+
+    })}
     </>
 }
