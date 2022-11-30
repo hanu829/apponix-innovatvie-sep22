5a7468fc9ff68f4b3ac7e2c9cf7c63a21bedadca
wa5 <47422872+wa5@users.noreply.github.com>
2022-11-30 08:45:27 +0530

    jjj

--- fullstack/frontend/library/flipkart-angular/src/app/Pages/home/home.component.ts
+++ fullstack/frontend/library/flipkart-angular/src/app/Pages/home/home.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+img1:string
+img2:string
+img3:string
+img4:string
+constructor(){
+this.img1='../../../assets/images/4.jpg'
+this.img2='../../../assets/images/2.jpg'
+this.img3='../../../assets/images/3.jpg'
+this.img4='../../../assets/images/1.jpg'
 
+}
 }
