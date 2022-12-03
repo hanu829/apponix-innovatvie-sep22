8739c52c794ef28581a10e0e19d0aa8cf57c82ce
wa5 <47422872+wa5@users.noreply.github.com>
2022-12-03 09:37:53 +0530

    kkk

--- fullstack/backend/library/nodetopics/emailsending/index.ts
+++ fullstack/backend/library/nodetopics/emailsending/index.ts
@@ -1,1 +1,21 @@
 var nodemailer=require('nodemailer')
+var transport=nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'waseemahmed116@gmail.com',
+        pass:'ur password'
+    }
+})
+var maildata={
+    from:'waseemahmed116@gmail.com',
+    to:'daluleelavathi@gmail.com',
+    subject:'testing u r awake',
+text:'hi can u dance'
+}
+transport.sendMail(maildata,(err:any,res:any)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('mail send')
+    }
+})
