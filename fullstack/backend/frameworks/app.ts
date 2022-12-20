e386cc3d7f2f4239d5b1ad42ef311a6909af2af3
wa5 <47422872+wa5@users.noreply.github.com>
2022-12-20 10:20:43 +0530

    we have done middlewares

--- fullstack/backend/frameworks/app.ts
+++ fullstack/backend/frameworks/app.ts
@@ -1,28 +1,19 @@
 var express=require('express')
 var app=express()
+var cors=require('cors')
+function middle(req:any,res:any,next:any){
+console.log('im called adveded')
+next()
+}
+app.use(middle)
+app.use(cors())
 
-app.get('/flight',(req:any,res:any)=>{
-    res.send('enjoy the party')
+app.get('/',(req:any,res:any)=>{
+    res.send([{a:'apple',b:'boll'}])
 })
-app.post('/user',(req:any,res:any)=>{
-    res.send('apponix student post')
+app.get('/a',(req:any,res:any)=>{
+    res.send([{a:'apple',b:'boll'}])
 })
-app.delete('/user',(req:any,res:any)=>{
-    res.send('apponix student delete')
-})
-app.put('/user',(req:any,res:any)=>{
-    res.send('apponix student put or update')
-})
-app.get('/user',(req:any,res:any)=>{
-    res.send('apponix student get')
-})
-app.get('/triler',(req:any,res:any)=>{
-    var triler='<iframe width="560" height="315" src="https://www.youtube.com/embed/5PSNL1qE6VY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-    res.send(triler)
-})
-
-
-
 
 app.listen(8003,()=>{
     console.log('server started on portt no 8003')
---
+++ fullstack/backend/frameworks/src/lessons/typesogApis.ts
@@ -0,0 +1,20 @@
+
+app.get('/flight',(req:any,res:any)=>{
+    res.send('enjoy the party')
+})
+app.post('/user',(req:any,res:any)=>{
+    res.send('apponix student post')
+})
+app.delete('/user',(req:any,res:any)=>{
+    res.send('apponix student delete')
+})
+app.put('/user',(req:any,res:any)=>{
+    res.send('apponix student put or update')
+})
+app.get('/user',(req:any,res:any)=>{
+    res.send('apponix student get')
+})
+app.get('/triler',(req:any,res:any)=>{
+    var triler='<iframe width="560" height="315" src="https://www.youtube.com/embed/5PSNL1qE6VY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    res.send(triler)
+})
