57777a955afe1c36be2081370f19b5586d00e47b
wa5 <47422872+wa5@users.noreply.github.com>
2022-12-22 10:35:15 +0530

    static path image

--- fullstack/backend/frameworks/app.ts
+++ fullstack/backend/frameworks/app.ts
@@ -2,42 +2,17 @@ var express=require('express')
 var app=express()
 var cors=require('cors')
 var bodyParser = require('body-parser')
+var path=require('path')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 app.use(cors())
 
-
-app.get('/index',(req:any,res:any)=>{
-res.send('<h1>hello</h1>')
-})
-
-app.get('/serverless',(req:any,res:any)=>{
-    res.redirect('http://127.0.0.1:5500/test.html')
-    })
+app.use(express.static(path.join(__dirname,'src/public/img')))
 
 
 
-
-//it lookes at strtingand ending in between u can give anyththing
-// app.get('/c*ars',(req:any,res:any)=>{
-//     res.send('hello')
-// })
-
-// app.get('/ca?rs',(req:any,res:any)=>{
-//     res.send('hello')
-// })
-//multiple path
-// app.get('/api/cars',(req:any,res:any)=>{
-//     res.send('hello')
-// })
-// app.get('/ca+rs',(req:any,res:any)=>{
-//     res.send('hello')
-// })
-// app.get('/cars/:id/:name',(req:any,res:any)=>{
-//     res.send('hello')
-// })
 
 
 
