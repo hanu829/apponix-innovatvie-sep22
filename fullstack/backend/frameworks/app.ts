23c961464b145613677241ff45a084e82747bc08
wa5 <47422872+wa5@users.noreply.github.com>
2022-12-22 10:17:26 +0530

    serverside and serverless

--- fullstack/backend/frameworks/app.ts
+++ fullstack/backend/frameworks/app.ts
@@ -2,32 +2,44 @@ var express=require('express')
 var app=express()
 var cors=require('cors')
 var bodyParser = require('body-parser')
-
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/cars',(req:any,res:any)=>{
-    console.log(req.query)
-    console.log(req.body)
 
-
-    //console.log(req.body)
-    res.send('helloS')
-})
-app.get('/cars',(req:any,res:any)=>{
-    console.log(req.query.username)
-    res.send(req.query.username)
+app.get('/index',(req:any,res:any)=>{
+res.send('<h1>hello</h1>')
 })
 
-app.get('/user/:id',(req:any,res:any)=>{
-    console.log(req.params.id)
-    res.send(req.params.id)
-})
+app.get('/serverless',(req:any,res:any)=>{
+    res.redirect('http://127.0.0.1:5500/test.html')
+    })
+
+
+
+
+//it lookes at strtingand ending in between u can give anyththing
+// app.get('/c*ars',(req:any,res:any)=>{
+//     res.send('hello')
+// })
+
+// app.get('/ca?rs',(req:any,res:any)=>{
+//     res.send('hello')
+// })
+//multiple path
+// app.get('/api/cars',(req:any,res:any)=>{
+//     res.send('hello')
+// })
+// app.get('/ca+rs',(req:any,res:any)=>{
+//     res.send('hello')
+// })
+// app.get('/cars/:id/:name',(req:any,res:any)=>{
+//     res.send('hello')
+// })
+
+
 
 
 app.listen(8003,()=>{
