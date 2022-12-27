b21ea0f6473c42ed1bc71d3545785d895302c82a
wa5 <47422872+wa5@users.noreply.github.com>
2022-12-27 10:30:47 +0530

    saparation of concerns dotenv db integeration

--- fullstack/backend/frameworks/app.ts
+++ fullstack/backend/frameworks/app.ts
@@ -1,9 +1,12 @@
-var express = require('express')
+require('dotenv').config()
+require("./config/db.config")
+import { express } from "./config/commom.config"
 var app = express()
 var cors = require('cors')
 var bodyParser = require('body-parser')
 var path = require('path')
 var ejs=require('ejs')
+var endpoint=require('./src/routes')
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname, 'src/views'))
 // parse application/x-www-form-urlencoded
@@ -15,20 +18,22 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'src/public/img')))
 app.use(express.static(path.join(__dirname, 'src/public/videos')))
 
+app.use('/api',endpoint.homeRoute)
+
+
 
 app.route('/')
 .get((req: any, res: any) => {
-    res.render('home')
-}).post((req: any, res: any) => {
-    res.send('hello')
-}).delete((req: any, res: any) => {
-    res.send('hello')
-}).put((req: any, res: any) => {
-    res.send('hello')
+    res.send('welcome')
+})
+app.route('*')
+.get((req: any, res: any) => {
+    
+    res.send({route:404})
 })
 
 
 
-app.listen(8003, () => {
-    console.log('server started on portt no 8003')
+app.listen(process.env.PORT, () => {
+    console.log(`server started on portt no ${process.env.PORT}`)
 })
