f76bbb26391c3322ba2bf92b4af279e7baeac254
waseem ahmed <waseemslyar@gmail.com>
2023-01-19 11:28:45 +0530

    java syntex

---
+++ fullstack/backend/language/java-language/learningJava/src/lessons/Lessons_01_syntax.java
@@ -0,0 +1,14 @@
+package lessons;
+
+public class Lessons_01_syntax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, lesson no v ");
+	}
+
+}
+class Human{
+
+}
+
---
+++ fullstack/backend/language/java-language/learningJava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Apponix
+ *
+ */
+module learningJava {
+}
