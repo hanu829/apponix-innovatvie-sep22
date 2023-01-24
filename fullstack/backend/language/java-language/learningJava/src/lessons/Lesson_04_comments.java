0b9d4031777e2af188138376fb8809947e43f46c
waseem ahmed <waseemslyar@gmail.com>
2023-01-24 11:31:11 +0530

    leant variables

---
+++ fullstack/backend/language/java-language/learningJava/src/lessons/Lesson_04_comments.java
@@ -0,0 +1,15 @@
+package lessons;
+
+public class Lesson_04_comments {
+
+	public static void main(String[] args) {
+		//single line comment --shortcut ctl+/
+//		int a;
+//		int b;
+		//int a=10;
+		//this is called multi line comment
+	/*	int a;
+		int b */
+	}
+	
+}
